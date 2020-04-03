330cef122adfd330c79254f7236cd2f35a42b8ad
Дмитрий <dkrovel@gmail.com>
2020-04-03 04:47:18 +0000

    Сделан базовый SPA блога * Сверстаны страницы регистрации, входа, домашная и о блоге * Создан prod шаблон и закинут в папку backend, для более удобной работы

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id = "app">
+    <Header class = "bg-black"></Header>
+    <section class = "main">
+      <router-view></router-view>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from "@/components/Index.vue"
+import Header from "@/components/Header.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index,
+    Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ frontend/src/components/Index.vue
@@ -0,0 +1,79 @@
+<template>
+    <b-container class = "main mt-3">
+        <h1 class = "main-title text-align-left">Последние статьи</h1>
+        <b-row>
+            <b-col cols = "9"  class = "article-list">
+                <articleItem></articleItem>
+            </b-col>
+            <b-col cols = "3" class = "article-category-list ml-auto">
+                <div class = "article-category-item">
+                    <h2 class = "article-category-title text-center">Категории</h2>
+                    <ul>
+                        <categoryItem></categoryItem>
+                    </ul>
+                </div>
+                <!-- <div class = "article-latest-item">
+                    <h2 class = "article-category-title text-center">Последние статьи</h2>
+                    <ul>
+                        <li><a href="#">О Vue</a></li>
+                        <li><a href="#">О Golang</a></li>
+                        <li><a href="#">О пиве</a></li>
+                    </ul>
+                </div> -->
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+<style>
+    .main-title{
+        margin: 20px 0 25px 0;
+    }
+    .main{
+        color: black;
+    }
+    .article-item,
+    .article-category-item,
+    .article-latest-item{
+        padding: 5px;
+        margin-bottom: 15px;
+        border: 1px solid rgba(177, 177, 177, 0.342);
+        border-radius: 10px;
+
+        color: white;
+
+        background-color: grey;
+    }
+
+    .article-item{
+        padding: 10px 15px 10px 15px;
+    }
+
+    .article-title{
+        margin: 15px 0;
+    }
+
+    .article-preview{
+        margin-bottom: 5px;
+    }
+
+    .article-info-likes{
+        cursor: pointer;
+    }
+
+    .article-info-comments,
+    .article-info-dislikes{
+        margin-left: 10px;
+    }
+</style>
+<script>
+    import articleItem from "@/components/Article-item.vue"
+    import categoryItem from "@/components/Category-item.vue"
+    
+    export default{
+        components:{
+            articleItem,
+            categoryItem
+        }
+    }
+//TODO: Реализовать загрузку статей на GitLab
+</script>
