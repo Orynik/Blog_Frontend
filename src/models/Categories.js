f3db4f298c26d4f31979f7c86dad04d2001d833d
Orynik <dkrovel@gmail.com>
2020-04-22 11:17:20 +0800

    Удаление ненужных для backend'а файлов

--- src/models/Categories.js
+++
@@ -1,9 +0,0 @@
-export default class Categories{
-    constructor(name){
-        this.name = name
-    }
-    static createCategories(data){
-        const {name} = data
-        return new this(name)
-    }
-}
