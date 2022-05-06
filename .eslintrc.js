b6b7fe65dce540f651c2bd9fe7d0bea215e86722
Orynik <dkrovel@gmail.com>
2022-05-07 00:39:28 +0800

    Change port golabg-rest to mock-rest

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+    jest: true
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-prototype-builtins': 'off',
+    'vue/no-unused-components': 2,
+    'vue/custom-event-name-casing': 'off',
+    'vue/multi-word-component-names': 0
+  },
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ],
+
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:promise/recommended',
+    '@vue/standard'
+  ]
+}
