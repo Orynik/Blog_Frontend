d0556760d765396ff7526c43741468f1771b9419
Orynik <dkrovel@gmail.com>
2022-05-07 10:26:29 +0800

    Setup preset SASS * For me: will need install saas package on linux

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,15 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/'
+    : '/',
+  rules: [
+    {
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }
+  ]
 }
