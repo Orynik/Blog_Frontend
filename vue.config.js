a05fe3e9f28bbfe5784b697b1a83c18bc5b335c8
Orynik <dkrovel@gmail.com>
2022-05-07 10:30:42 +0800

    Comment SASS setup

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,20 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
+  //  FIXME: Will check with sass package
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.scss$/,
+  //       use: [
+  //         'vue-style-loader',
+  //         'css-loader',
+  //         'sass-loader'
+  //       ]
+  //     }
+  //   ]
+  // },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/',
-  rules: [
-    {
-      test: /\.scss$/,
-      use: [
-        'vue-style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }
-  ]
+    : '/'
 }
